acfce9558bbe0c1a86ea8d2c7108c9c1583b8f8c
Wyatt Focht <wyatt.focht@duke.edu>
2020-09-19 12:59:25 -0400

    Created framework for PowerUp class.  Added movePowerUp to control movement of power up

---
+++ src/breakout/PowerUp.java
@@ -0,0 +1,32 @@
+package breakout;
+
+import javafx.scene.Group;
+import javafx.scene.shape.Circle;
+
+public class PowerUp extends Circle {
+
+  //constants
+  private static final double POWER_UP_RADIUS = 5;
+  private static final int POWER_UP_SPEED = 50;
+  private static final int MY_Y_DIRECTION = 1;
+  //instance variables
+  private Group myRoot;
+  private boolean paused;
+  private int screenHeight;
+
+  public PowerUp(double initialX, double initialY, Group myRoot, int screenHeight) {
+    super(initialX, initialY, POWER_UP_RADIUS);
+    this.screenHeight = screenHeight;
+    this.paused = false;
+    this.myRoot = myRoot;
+  }
+
+  public void movePowerUp(double elapsedTime) {
+    if (!paused) {
+      if (this.getCenterY() - this.getRadius() >= screenHeight) {
+        myRoot.getChildren().remove(this);
+      }
+      this.setCenterY(this.getCenterY() + MY_Y_DIRECTION * POWER_UP_SPEED * elapsedTime);
+    }
+  }
+}
